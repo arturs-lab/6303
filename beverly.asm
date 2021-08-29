a11f013f2dff4b3fc5441b1e9c7f28863404fe3a
arturj <arturj@admin>
2021-08-28 21:32:07 -0500

    move music to include files

---
+++ beverly.asm
@@ -0,0 +1,350 @@
+song	dc.w $1800	; playback speed
+	dc.w chc
+	dc.w chb
+cha
+	dc.b 32,5
+	dc.b 35,4
+	dc.b 35,3
+	dc.b 32,4
+;	dc.b  0,1
+	dc.b 32,3
+	dc.b 37,4
+	dc.b 32,4
+	dc.b 30,4
+
+	dc.b 32,5
+	dc.b 39,4
+	dc.b 39,3
+	dc.b 32,4
+;	dc.b  0,1
+	dc.b 32,3
+	dc.b 40,4
+	dc.b 39,4
+	dc.b 35,4
+
+	dc.b 32,4
+	dc.b 39,4
+	dc.b 44,4
+	dc.b 32,3
+;	dc.b  0,1
+	dc.b 30,4
+;	dc.b  0,1
+	dc.b 30,3
+	dc.b 27,4
+	dc.b 34,4
+	dc.b 32,4
+
+	dc.b 32,6
+	dc.b  0,6
+
+	dc.b 32,5
+	dc.b 35,4
+	dc.b 35,3
+	dc.b 32,4
+;	dc.b  0,1
+	dc.b 32,3
+	dc.b 37,4
+	dc.b 32,4
+	dc.b 30,4
+
+	dc.b 32,5
+	dc.b 39,4
+	dc.b 39,3
+	dc.b 32,4
+;	dc.b  0,1
+	dc.b 32,3
+	dc.b 40,4
+	dc.b 39,4
+	dc.b 35,4
+
+	dc.b 32,4
+	dc.b 39,4
+	dc.b 44,4
+	dc.b 32,3
+;	dc.b  0,1
+	dc.b 30,4
+;	dc.b  0,1
+	dc.b 30,3
+	dc.b 27,4
+	dc.b 34,4
+	dc.b 32,4
+
+	dc.b 32,6
+	dc.b  0,6
+
+	dc.b 0,7
+	dc.b 0,7
+	dc.b 0,7
+	dc.b 0,7
+
+	dc.b 0,7
+	dc.b 0,7
+	dc.b 0,7
+	dc.b 0,7
+
+	dc.b 32,5
+	dc.b 35,4
+	dc.b 35,3
+	dc.b 32,4
+;	dc.b  0,1
+	dc.b 32,3
+	dc.b 37,4
+	dc.b 32,4
+	dc.b 30,4
+
+	dc.b 32,5
+	dc.b 39,4
+	dc.b 39,3
+	dc.b 32,4
+;	dc.b  0,1
+	dc.b 32,3
+	dc.b 40,4
+	dc.b 39,4
+	dc.b 35,4
+
+	dc.b 32,4
+	dc.b 39,4
+	dc.b 44,4
+	dc.b 32,3
+;	dc.b  0,1
+	dc.b 30,4
+;	dc.b  0,1
+	dc.b 30,3
+	dc.b 27,4
+	dc.b 34,4
+	dc.b 32,4
+
+	dc.b 32,6
+	dc.b  0,6
+
+	dc.b 32,5
+	dc.b 35,4
+	dc.b 35,3
+	dc.b 32,4
+;	dc.b  0,1
+	dc.b 32,3
+	dc.b 37,4
+	dc.b 32,4
+	dc.b 30,4
+
+	dc.b 32,5
+	dc.b 39,4
+	dc.b 39,3
+	dc.b 32,4
+;	dc.b  0,1
+	dc.b 32,3
+	dc.b 40,4
+	dc.b 39,4
+	dc.b 35,4
+
+	dc.b 32,4
+	dc.b 39,4
+	dc.b 44,4
+	dc.b 32,3
+;	dc.b  0,1
+	dc.b 30,4
+;	dc.b  0,1
+	dc.b 30,3
+	dc.b 27,4
+	dc.b 34,4
+	dc.b 32,4
+
+	dc.b 32,6
+	dc.b  0,6
+
+	dc.b 0,7
+
+	dc.b 255,255
+
+chb
+	dc.b 0,7
+	dc.b 0,7
+	dc.b 0,7
+	dc.b 0,7
+
+	dc.b 0,7
+	dc.b 0,7
+	dc.b 0,7
+	dc.b 0,7
+
+	dc.b 8,5
+	dc.b 20,4
+	dc.b 20,3
+	dc.b 6,4
+	dc.b 18,3
+	dc.b 3,4
+	dc.b 15,4
+	dc.b 6,4
+
+	dc.b 8,5
+	dc.b 20,3
+	dc.b 0,5
+	dc.b 3,3
+;	dc.b 0,1
+	dc.b 3,4
+	dc.b 6,4
+	dc.b 8,4
+
+	dc.b 4,5
+	dc.b 16,4
+	dc.b 16,3
+	dc.b 6,4
+	dc.b 18,3
+	dc.b 15,4
+	dc.b 18,4
+	dc.b 8,4
+
+	dc.b 20,5
+	dc.b 0,5
+	dc.b 0,3
+	dc.b 18,3
+	dc.b 15,4
+	dc.b 13,4
+	dc.b 11,4
+
+	dc.b 8,5
+	dc.b 20,4
+	dc.b 20,3
+	dc.b 6,4
+	dc.b 18,3
+	dc.b 3,4
+	dc.b 15,4
+	dc.b 6,4
+
+	dc.b 8,5
+	dc.b 20,3
+	dc.b 0,5
+	dc.b 3,3
+;	dc.b 0,1
+	dc.b 3,4
+	dc.b 6,4
+	dc.b 8,4
+
+	dc.b 4,5
+	dc.b 16,4
+	dc.b 16,3
+	dc.b 6,4
+	dc.b 18,3
+	dc.b 15,4
+	dc.b 18,4
+	dc.b 8,4
+
+	dc.b 20,5
+	dc.b 0,5
+	dc.b 0,3
+	dc.b 18,3
+	dc.b 15,4
+	dc.b 13,4
+	dc.b 11,4
+
+	dc.b 8,5
+	dc.b 20,4
+	dc.b 20,3
+	dc.b 6,4
+	dc.b 18,3
+	dc.b 3,4
+	dc.b 15,4
+	dc.b 6,4
+
+	dc.b 8,5
+	dc.b 20,3
+	dc.b 0,5
+	dc.b 3,3
+;	dc.b 0,1
+	dc.b 3,4
+	dc.b 6,4
+	dc.b 8,4
+
+	dc.b 4,5
+	dc.b 16,4
+	dc.b 16,3
+	dc.b 6,4
+	dc.b 18,3
+	dc.b 15,4
+	dc.b 18,4
+	dc.b 8,4
+
+	dc.b 20,5
+	dc.b 0,5
+	dc.b 0,3
+	dc.b 18,3
+	dc.b 15,4
+	dc.b 13,4
+	dc.b 11,4
+
+	dc.b 8,5
+	dc.b 20,4
+	dc.b 20,3
+	dc.b 6,4
+	dc.b 18,3
+	dc.b 3,4
+	dc.b 15,4
+	dc.b 6,4
+
+	dc.b 8,5
+	dc.b 20,3
+	dc.b 0,5
+	dc.b 3,3
+;	dc.b 0,1
+	dc.b 3,4
+	dc.b 6,4
+	dc.b 8,4
+
+	dc.b 4,5
+	dc.b 16,4
+	dc.b 16,3
+	dc.b 6,4
+	dc.b 18,3
+	dc.b 15,4
+	dc.b 18,4
+	dc.b 8,4
+
+	dc.b 20,5
+	dc.b 0,5
+	dc.b 0,3
+	dc.b 18,3
+	dc.b 15,4
+	dc.b 13,4
+	dc.b 11,4
+
+	dc.b 0,7
+	dc.b 0,7
+
+	dc.b 254,255
+
+chc
+	dc.b 0,7
+	dc.b 0,7
+	dc.b 0,7
+	dc.b 0,7
+
+	dc.b 0,7
+	dc.b 0,7
+	dc.b 0,7
+	dc.b 0,7
+
+	dc.b 0,7
+	dc.b 0,7
+	dc.b 0,7
+	dc.b 0,7
+
+	dc.b 0,7
+	dc.b 0,7
+	dc.b 0,7
+	dc.b 0,7
+
+	dc.b 0,7
+	dc.b 0,7
+	dc.b 0,7
+	dc.b 0,7
+
+	dc.b 0,7
+	dc.b 0,7
+	dc.b 0,7
+	dc.b 0,7
+
+	dc.b 0,7
+	dc.b 0,7
+
+	dc.b 254,255
+
